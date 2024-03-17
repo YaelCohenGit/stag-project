2fcb32581f0ce1e2fe69b4e9a98fe55d43652b75
YaelCohenGit <yael504030@gmail.com>
2024-03-17 20:17:27 +0200

    OwnerToAptDetailsRepo update

--- Booking/BL/BLImplementation/OwnerToAptDetailsRepo.cs
+++ Booking/BL/BLImplementation/OwnerToAptDetailsRepo.cs
@@ -20,5 +20,39 @@ namespace BL.BLImplementation
             this.AptDetail = dalManager.AptDetail;
         }
 
+        public async Task<Owner> AddOwner(Owner owner)
+        {
+            Owner newOwner = new Owner(owner.Tel, owner.Email ,owner.AptDetails);
+            await ownersRepo.AddAsync(newOwner);
+            return newOwner;
+        }
+
+        //public List<Owner> GetOwners(BaseQueryParams queryParams)
+        //{
+        //    //Task<PagedList<Owner>> pagedOwners = ownersRepo.GetSingleAsync(queryParams);
+        //    List<Owner> OwnersList = new List<Owner>();
+        //    //foreach (var Owner in pagedOwners.Result)
+        //    {
+        //        Owner newOwner = new Owner();
+        //        //newOwner.FirstName = Owner.Name.Split(' ')[0];
+        //        //newOwner.LastName = Owner.Name.Split(" ")[1];
+        //        //newOwner.Email = Owner.Email;
+        //        //newOwner.Password = Owner.Password;
+        //        OwnersList.Add(newOwner);
+        //    }
+        //    return OwnersList;
+        //}
+
+        public Owner GetById(int id)
+        {
+            Task<Owner> Owner = ownersRepo.GetSingleAsync(id);
+            Owner newOwner = new Owner();
+            //newOwner.FirstName = Owner.Result.Name.Split(' ')[0];
+            //newOwner.LastName = Owner.Result.Name.Split(" ")[1];
+            //newOwner.Email = Owner.Result.Email;
+            //newOwner.Password = Owner.Result.Password;
+            return newOwner;
+        }
+
     }
 }
