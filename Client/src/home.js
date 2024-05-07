9071916bb9d0ceb61720d0eefb358c3275a85886
אינהורן חיה < @mbjcomp.local>
2024-05-07 12:48:23 +0300

    install bootstrap

---
+++ Client/src/home.js
@@ -0,0 +1,27 @@
+
+// import './home.css';
+
+// function Home() {
+//   return (
+//     <div className="">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <p>
+//           Wel
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+
+// <button type="button" class="btn btn-outline-dark">Dark</button>
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// } 
+
+// export default Home;
